758bb6fe075ae5508514e404bd0ec4cf01e98ef1
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-10-13 15:21:14 -0300

    correcting errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
 import './styles/pages/landing.css';
@@ -21,8 +22,8 @@ function App() {
           <span>Rio de Janeiro</span>
         </div>
 
-        <a href="" className="enter-app">
-          >
+        <a className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
         </a>
       </div>
     </div>
