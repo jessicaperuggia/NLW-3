23c806708784d8a85e635643f2edfba87b7e129d
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-10-13 16:54:26 -0300

    stylizing OrphanagesMap

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../img/map-marker.svg';
+
+import '../styles/pages/orphanege-map.css';
 
 function OrphanagesMap() {
     return (
-        <div>
-            <h1>oi</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+
+                    <h2>Escolha um orfanatono mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Niterói</strong>
+                    <span>Rio de Janeiro</span>
+                </footer>
+            </aside>
+            <div></div>
+
+            <Link to="" className="create-orphanege">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
         </div>
     )
 }
