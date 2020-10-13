03a31ba9aad750e3dca94222607fa253d865d82a
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-10-13 17:53:50 -0300

    placing the map on OrphanagesMap

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../img/map-marker.svg';
 
-import '../styles/pages/orphanege-map.css';
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return (
@@ -21,9 +24,19 @@ function OrphanagesMap() {
                     <span>Rio de Janeiro</span>
                 </footer>
             </aside>
-            <div></div>
+            <Map
+                center={[-22.9073482, -43.1065697]}
+                zoom={14.25}
+                style={{ width: '100%', height: '100%' }}>
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
 
-            <Link to="" className="create-orphanege">
+            </Map>
+
+
+
+            <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
